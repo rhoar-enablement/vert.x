1465537036a753aeab9ffc2daa2d414d4bed46fe
Chad Darby <cdarby@redhat.com>
2017-08-10 17:24:06 -0400

    updates for lab1

---
+++ lab1/src/main/java/io/vertx/sample/MyFirstVerticle.java
@@ -0,0 +1,12 @@
+package io.vertx.sample;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MyFirstVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
